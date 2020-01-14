d1b77acb0a5ee413c0d4970bf7d56be56ff19972
m-naim <mehenaouin@gmail.com>
2020-01-14 13:16:25 +0100

    rest update

--- src/main/java/com/example/accessingdatajpa/CustomerRepository.java
+++ src/main/java/com/example/accessingdatajpa/CustomerRepository.java
@@ -2,11 +2,13 @@ package com.example.accessingdatajpa;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
 
-  List<Customer> findByLastName(String lastName);
+  List<Customer> findByLastName(@Param("name") String name);
 
-  Customer findById(long id);
 }
