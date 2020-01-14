5f842cab87947d607f649045bef1e94f37dbdf6e
m-naim <mehenaouin@gmail.com>
2020-01-14 15:23:56 +0100

    jpa rest end

--- src/main/java/com/example/accessingdatajpa/CustomerRepository.java
+++ src/main/java/com/example/accessingdatajpa/CustomerRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+@RepositoryRestResource(collectionResourceRel = "Customers", path = "Customers")
 public interface CustomerRepository extends PagingAndSortingRepository<Customer, Long> {
 
   List<Customer> findByLastName(@Param("name") String name);
