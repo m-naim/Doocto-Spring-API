ae004fbc67d77c07b8f1609af70bbdac1da8e38c
m-naim <mehenaouin@gmail.com>
2021-02-12 15:19:01 +0100

    cross domaine

--- src/main/java/org/naim/doctoo/config/WebMvcConfig.java
+++ src/main/java/org/naim/doctoo/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-        .allowedOrigins("*")
+        .allowedOrigins("http://localhost:4200/","https://tabibrdv.com/")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true)
